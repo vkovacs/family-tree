f580247b6c6a857cba502c88e23ca937720cbb46
Viktor Kovacs <crusader8@gmail.com>
2022-12-03 09:33:14 +0100

    remove unnecessary file

--- src/main/groovy/hu/crs/family/familytree/application/domain/Member.groovy
+++ src/main/groovy/hu/crs/family/familytree/application/domain/Member.groovy
@@ -2,14 +2,12 @@ package hu.crs.family.familytree.application.domain
 
 import groovy.transform.Immutable
 
-import java.time.LocalDate
-
 @Immutable
 class Member {
     String id = UUID.randomUUID().toString()
     String name
     Member mother
     Member father
-    LocalDate dateOfBirth
-    LocalDate dateOfDeath
+    int yearOfBirth
+    int yearOfDeath
 }
